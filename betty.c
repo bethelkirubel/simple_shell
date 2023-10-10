b4e273ac4fd251b9de0e54ca7fd9b567726700dc
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-10-09 18:29:51 -0700

    added my first commit

---
+++ betty.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints 18
+ * Return: nothing
+ */
+
+int main(void)
+{
+printf("%d", 18);
+}
---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fnctl.h>
+#include <unistd.h>
+#include <strings.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+
+
+#endif
+
